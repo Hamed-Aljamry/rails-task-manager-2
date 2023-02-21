676d8cea3359e0b8bd97f9180ad57e503ac1d429
hamed aljamry <hamed970@hotmail.com>
2023-02-21 00:23:32 +0000

    update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,4 +10,27 @@ class TasksController < ApplicationController
   def new
     @task = Task.new # Needed to instantiate the form_with
   end
+
+  def create
+    task = Task.new(task_params)
+    task.save
+    # No need for app/views/restaurants/create.html.erb
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
